326896b888df315ced3048928a8dc9fd03a9693d
Simon Alexander Alsing <SAL@kamstrup.com>
2019-10-08 17:32:01 +0200

    Fix BinaryTreeSearch for too long code path

--- src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
+++ src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
@@ -2,29 +2,18 @@ package eu.sig.training.ch03.binarytree;
 
 public class BinaryTreeSearch {
 
-    // tag::calculateDepth[]
     public static int calculateDepth(BinaryTreeNode<Integer> node, int nodeValue) {
         int depth = 0;
         if (node.getValue() == nodeValue) {
             return depth;
+        }
+
+        BinaryTreeNode<Integer> value = (nodeValue < node.getValue()) ? node.getLeft():node.getRight();
+
+        if (value == null) {
+            throw new TreeException("Value not found in tree!");
         } else {
-            if (nodeValue < node.getValue()) {
-                BinaryTreeNode<Integer> left = node.getLeft();
-                if (left == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(left, nodeValue);
-                }
-            } else {
-                BinaryTreeNode<Integer> right = node.getRight();
-                if (right == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(right, nodeValue);
-                }
-            }
+            return 1 + calculateDepth(value, nodeValue);
         }
     }
-    // end::calculateDepth[]
-
 }
